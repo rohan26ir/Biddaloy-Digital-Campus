baab2c3bd3513140846103782b7130b053c3ed56
rohan26ir <rohan26ir@gmail.com>
2026-04-10 22:01:50 +0600

    feat(navbar), add navbar and setup color and size

---
+++ app/(public)/page.tsx
@@ -0,0 +1,21 @@
+
+
+
+
+
+export default function Home() {
+  return (
+    <div className="py-20">
+      <div className="max-w-7xl w-[95%] mx-auto">
+
+        <div>
+          <h1 className="text-3xl font-bold ">
+            Hello world!
+          </h1>
+        </div>
+
+
+      </div>
+    </div>
+  )
+}
